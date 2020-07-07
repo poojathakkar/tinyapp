e0bc242d512d757b450a0f562f18b77a9c5267c8
Pooja Thakkar <poojapthakkar@gmail.com>
2020-07-07 03:30:59 +0000

    added redirect

--- express_server.js
+++ express_server.js
@@ -41,18 +41,28 @@ app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString();
+  const longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  console.log(req.body);  // Log the POST request body to the console
+  console.log(longURL);
+  console.log(urlDatabase);
+  //res.send("OK");
+  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-app.post("/urls", (req, res) => {
-  const shortURL = generateRandomString();
-  const longURL = req.body.longURL;
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("OK");
-  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
-});
+
+
+/*app.get("/u/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.redirect(templateVars.longURL);
+});*/
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
